447b468eb0686397c759dd3ce606dc96c9a6922c
VictoriaDiro <victoriadiro01@gmail.com>
2019-04-08 08:26:10 +0200

    initial structure

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// generar un número al alzar entre 1 y 100
+// el jugador tiene que adivinar
+// en h2 se imprime si el número del input es mayor o menor, o si lo ha conseguido
+// el contador de la derecha cuenta el número de intentos
+// cuando acierte el juego se termina
+// al pulsar el botón test acceder a input y mostrar en cosola
+
+
+  

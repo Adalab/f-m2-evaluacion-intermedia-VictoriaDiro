5d2f45f5ef75a8d6720a6f0366bb1438836836dc
VictoriaDiro <victoriadiro01@gmail.com>
2019-04-08 15:01:07 +0200

    update exercise

--- main.js
+++ main.js
@@ -5,27 +5,33 @@ let testButton = document.querySelector('.test__button');
 const check = document.querySelector('.check');
 const acc = document.querySelector('.acc')
 
+// let i = 0 // inicializador para contadir
+// i = i+1
+// counter.innerHTML = i // dentro de condicional cuando no es igual
+
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * 100);
   }
   console.log('> ' + getRandomNumber(100));
 
-function checkYourNumber(getRandomNumber) {
-    if (inputNumber.value === getRandomNumber) {
-        check.innerText = '¡HAS GANADO CAMPEONA!';
-    } else if (inputNumber.value < getRandomNumber) {
-        check.innerText = 'Demasiado bajo';
+function game() {
+    const userNumber = parseInt(inputNumber.value)
+    if (userNumber > getRandomNumber) {
+        check.innerHTML = 'Demasiado alto';
+    } else if (userNumber < getRandomNumber) {
+        check.innerHTML = 'Demasiado bajo';
     } else {
-        check.innerText = 'Demasiado alto';
+        check.innerHTML = '¡HAS GANADO CAMPEONA!';
     }
 }
 
-function countClick() {
-    testButton += 1;
-    countClick = acc.innerHTML;
-}
 
-testButton.addEventListener('click', checkYourNumber);
+// function countClick() {
+//     testButton += 1;
+//     countClick = acc.innerHTML;
+// }
+
+testButton.addEventListener('click', game);
 
 testButton.addEventListener('click',testButton);
 
